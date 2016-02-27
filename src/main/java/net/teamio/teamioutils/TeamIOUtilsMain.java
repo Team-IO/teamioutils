1ce8554ae4332e32963c84587327b61d9e5288e6
Alexander Sieg <xander.lp112@gmail.com>
2016-02-27 22:04:55 +0100

    Recipe TIME

--- src/main/java/net/teamio/teamioutils/TeamIOUtilsMain.java
+++ src/main/java/net/teamio/teamioutils/TeamIOUtilsMain.java
@@ -1,5 +1,8 @@
 package net.teamio.teamioutils;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -7,6 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 @Mod(modid = TeamIOUtils.MOD_ID, version = TeamIOUtils.VERSION)
 public class TeamIOUtilsMain
@@ -24,6 +28,11 @@ public class TeamIOUtilsMain
 		
 		itemHammer = (Hammer)(new Hammer().setUnlocalizedName(TeamIOUtils.ITEM_HAMMER));
 		GameRegistry.registerItem(itemHammer, TeamIOUtils.ITEM_HAMMER);
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(itemHammer, 1, 0),
+				"DID", "DSD", "ESE",
+				'D', Blocks.diamond_block,
+				'I', Blocks.iron_block,
+				'S', Items.stick));
 	}
     
     @EventHandler
